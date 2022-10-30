1cddebfd9817a50561ccee39f0b9768d5586bb89
Selami Demir <selamidemir@gmail.com>
2022-10-30 18:03:06 +0300

    Footer düzenlendi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 function Footer() {
   return (
-    <Container fluid className='footer d-flex justify-content-center p-3 mt-3 border-top shadow'>
-        2022 @ Developed By <a href='https://github.com/selamidemir'>Selami Demir</a>
+    <Container
+      fluid
+      className="footer d-flex justify-content-center p-3 mt-3 border-top shadow"
+    >
+      2022 @ Developed By &nbsp;{" "}
+      <a href="https://github.com/selamidemir"> Selami Demir</a>
     </Container>
-  )
+  );
 }
 
 export default Footer;
